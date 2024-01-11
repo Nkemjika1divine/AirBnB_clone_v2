caa89a2b111a4878c96094a38ce519e6fd353e36
Nkemjika1divine <cndivine@gmail.com>
2024-01-11 10:59:47 -0800

    Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,7 @@ mkdir -p /data
 mkdir -p /data/web_static
 mkdir -p /data/web_static/releases
 mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
 echo "My web server" > /data/web_static/releases/test/index.html
 ln -sfn /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
