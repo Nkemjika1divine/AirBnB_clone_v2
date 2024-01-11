a41ceb4f1e15ea657fad79d20e4028d28784b9ab
Nkemjika1divine <cndivine@gmail.com>
2024-01-11 10:38:10 -0800

    Deploy static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# preforms certain functions
+sudo apt-get -y update
+sudo apt-get -y install nginx
+mkdir -p /data
+mkdir -p /data/web_static
+mkdir -p /data/web_static/releases
+mkdir -p /data/web_static/releases/test
+echo "My web server" > /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+old="server_name _;"
+new="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "s|$old|$new|" /etc/nginx/sites-enabled/default
+sudo service nginx restart
