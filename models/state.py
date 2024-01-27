65740b5cb5e6707e25db8f90b6b792c584761fba
Nkemjika1divine <cndivine@gmail.com>
2024-01-27 00:00:31 -0800

    Web Flask project

--- models/state.py
+++ models/state.py
@@ -8,15 +8,19 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
 
-    cities = relationship(
-        "City",
-        cascade="all,delete,delete-orphan",
-        backref=backref("state", cascade="all,delete"),
-        passive_deletes=True,
-        single_parent=True)
+    if storage_type == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+
+        cities = relationship(
+            "City",
+            cascade="all,delete,delete-orphan",
+            backref=backref("state", cascade="all,delete"),
+            passive_deletes=True,
+            single_parent=True)
+    else:
+        name = ""
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
